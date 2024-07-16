d1eee88ad69ea715596e36940a64a72af125c1b7
Gabriel Santos Oliveira <santosgabriel28@hotmail.com>
2024-07-16 20:13:49 -0300

    Better comments, rewrite of add_node
    There were some points of improvement on the add_node function, so
    I rewrote it in a more compact form which is still readable: this
    should be easier to parse visually and also more performant. I do
    need to run some tests on this function to ensure that it retains
    its original funcionality.

--- prune_petri.m
+++ prune_petri.m
@@ -14,20 +14,19 @@ function new_net = prune_petri(petri_net_matrix)
 endfunction
 
 function new_net = del_edge(petri_net_matrix)
-  % Find the rows that move at least three tokens. The last column represents
-  % the net marking, so we can discard it.
+  % Find places that are connected to at least three transitions
   row_totals = sum(petri_net_matrix(:, 1:end-1), 2);
   row_idxs = find(row_totals >= 3);
-  % Select sum - 2 random indexes from each row and set them to 0 so that they
-  % only move one token. Octave iterates over columns, so we have to transpose
-  % the row_idxs since it is a column vector.
+  % Make it so the place is only connected to a single transition.
+  % Octave iterates over columns, so we have to transpose the row_idxs since it
+  % is a column vector.
   for row = row_idxs'
     idxs = find(petri_net_matrix(row, 1:end-1) == 1);
     rm_idxs = random_choice(idxs, row_totals(row) - 2);
     petri_net_matrix(row, rm_idxs) = 0;
   endfor
 
-  % Do the same process, but for the columns this time.
+  % Do the same process for transitions
   column_totals = sum(petri_net_matrix(:, 1:end-1), 1);
   column_idxs = find(column_totals >= 3);
   % column_idxs is already a row vector, so there is no need to transpose it.
@@ -40,34 +39,23 @@ function new_net = del_edge(petri_net_matrix)
 endfunction
 
 function new_net = add_node(petri_net_matrix)
-  column_size = (columns(petri_net_matrix) - 1)/2;
-  % Each transition must move at least one token, so we need to find columns
-  % whose elements are all zero. The last column contains the marking, so we
-  % discard it.
-  column_idxs = find(all(~petri_net_matrix(:, 1:end-1), 1));
-  % If no elements match, the find function returns an empty matrix.
-  if (~isempty(column_idxs)
-    choices = randi(column_size, sum(column_idxs));
-    petri_net_matrix(choices, column_idxs) = 1;
-  endif
+  % Each (transition) column must have at least one connection
+  column_idxs = find(all(petri_net_matrix == 0, 1));
+  num_rows = rows(petri_net_matrix);
+  for idx = column_idxs
+    petri_net_matrix(randi(num_rows), idx);
+  endfor
 
-  tran_num = (columns(petri_net_matrix) - 1)/2;
-  left_matrix = petri_net_matrix(:, 1:tran_num);
-  right_matrix = petri_net_matrix(:, (tran_num + 1):(end - 1));
-  % Each row in the left matrix must move at least one token, so we find the
-  % rows whose elements are all zero.
-  row_idxs = all(~left_matrix, 2);
-  if (~isempty(row_idxs))
-    choices = randi(column_size, numel(row_idxs));
-    left_matrix(row_idxs, choices) = 1;
-  endif
-  % The same thing for the right matrix.
-  row_idxs = all(~right_matrix, 2);
-  if (~isempty(row_idxs))
-    choices = randi(column_size, numel(row_idxs));
-    right_matrix(row_idxs, choices) = 1;
-  endif
-  % Concatenate these matrices horizontaly. We also need to add the initial
-  % marking back into this matrix.
-  new_net = [left_matrix, right_matrix, petri_net_matrix(:, end)];
+  % Each (place) row must have at least one connection.
+  num_transitions = (columns(petri_net_matrix) - 1)/2;
+  left_matrix = petri_net_matrix(:, 1:num_transitions);
+  row_idxs = find(all(petri_net_matrix == 0, 2));
+  % If we add 1 on the left, we must also add it to the right
+  for row = row_idxs'
+    choice = randi(num_transitions);
+    petri_net_matrix(choice, row) = 1;
+    petri_net_matrix(choice, row + num_transitions) = 1;
+  endfor
+
+  new_net = petri_net_matrix;
 endfunction
